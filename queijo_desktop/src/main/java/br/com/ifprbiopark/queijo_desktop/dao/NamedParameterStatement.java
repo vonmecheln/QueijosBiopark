2888e047e3428ca6cd34a5a14337e366bf25f985
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-11-01 16:55:40 -0300

    Correções segundo Mauda

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/NamedParameterStatement.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/NamedParameterStatement.java
@@ -1,136 +1,120 @@
 package br.com.ifprbiopark.queijo_desktop.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Timestamp;
+
+
+/*
+* @author adam_crume customized by Mauda
+* http://mauda.com.br/?p=827
+ */
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-
-/*
-* @author adam_crume
- */
 public class NamedParameterStatement {
 
-    /**
-     * The statement this object is wrapping.
-     */
-    private final PreparedStatement statement;
+    ////////////////////////////////////////////////////////////////
+    // ATRIBUTOS
+    ////////////////////////////////////////////////////////////////
+    //Cria um PreparedStatement interno para realizar a execucao do SQL
+    private PreparedStatement statement;
 
-    /**
-     * Maps parameter names to arrays of ints which are the parameter indices.
-     */
-    private final Map indexMap;
+    //Map para indicar as posicoes dos parametros
+    private final Map<String, List<Integer>> mapPosicoesParametros = new HashMap<>();
 
+    ////////////////////////////////////////////////////////////////
+    // CONSTRUTORES
+    ////////////////////////////////////////////////////////////////
     /**
-     * Creates a NamedParameterStatement. Wraps a call to
-     * c.{@link Connection#prepareStatement(java.lang.String) prepareStatement}.
+     * Construtor que realiza a instanciacao de um prepareStatement a partir da
+     * connection
      *
-     * @param connection the database connection
-     * @param query the parameterized query
-     * @throws SQLException if the statement could not be created
+     * @param connection - A conexao com o banco de dados
+     * @param namedQuery - A query no formato namedQuery
+     * @throws SQLException - Se o statement nao puder ser criado
      */
-    public NamedParameterStatement(Connection connection, String query) throws
-            SQLException {
-        indexMap = new HashMap();
-        String parsedQuery = parse(query, indexMap);
+    public NamedParameterStatement(Connection connection, String namedQuery) throws SQLException {
+        String parsedQuery = parse(namedQuery);
         statement = connection.prepareStatement(parsedQuery);
     }
 
     /**
-     * Creates a NamedParameterStatement. Wraps a call to
-     * c.{@link Connection#prepareStatement(java.lang.String) prepareStatement}.
+     * Construtor que realiza a instanciacao de um prepareStatement a partir da
+     * connection para inserts com auto generated keys
      *
-     * @param connection the database connection
-     * @param query the parameterized query
-     * @param RETURN_GENERATED_KEYS	- A constante definida em
+     * @param connection - A conexao com o banco de dados
+     * @param namedQuery - A query no formato namedQuery
+     * @param RETURN_GENERATED_KEYS - A constante definida em
      * java.sql.Statement.RETURN_GENERATED_KEYS
      * @throws SQLException - Se o statement nao puder ser criado
      */
-    public NamedParameterStatement(Connection connection, String query,
-            Integer RETURN_GENERATED_KEYS) throws SQLException {
-        indexMap = new HashMap();
-        String parsedQuery = parse(query, indexMap);
+    public NamedParameterStatement(Connection connection, String namedQuery, Integer RETURN_GENERATED_KEYS) throws SQLException {
+        String parsedQuery = parse(namedQuery);
         statement = connection.prepareStatement(parsedQuery, RETURN_GENERATED_KEYS);
     }
 
-    /**
-     * Parses a query with named parameters. The parameter-index mappings are
-     * put into the map, and the parsed query is returned. DO NOT CALL FROM
-     * CLIENT CODE. This method is non-private so JUnit code can test it.
-     *
-     * @param query query to parse
-     * @param paramMap map to hold parameter-index mappings
-     * @return the parsed query
-     */
-    static final String parse(String query, Map paramMap) {
-        // I was originally using regular expressions, but they didn't work well for ignoring 
-        // parameter-like strings inside quotes.
-        int length = query.length();
+    ////////////////////////////////////////////////////////////////
+    // PARSER
+    ////////////////////////////////////////////////////////////////
+    private String parse(String namedQuery) {
+        int length = namedQuery.length();
+
+        //Cria um String Buffer com o tamanho do SQL
         StringBuilder parsedQuery = new StringBuilder(length);
+
         boolean inSingleQuote = false;
         boolean inDoubleQuote = false;
-        int index = 1;
 
+        int position = 1;
+        //Percorre todo o SQL
         for (int i = 0; i < length; i++) {
-            char c = query.charAt(i);
-            if (inSingleQuote) {
-                if (c == '\'') {
-                    inSingleQuote = false;
-                }
-            } else if (inDoubleQuote) {
-                if (c == '"') {
-                    inDoubleQuote = false;
-                }
-            } else {
-                if (c == '\'') {
-                    inSingleQuote = true;
-                } else if (c == '"') {
-                    inDoubleQuote = true;
-                } else if (c == ':' && i + 1 < length
-                        && Character.isJavaIdentifierStart(query.charAt(i + 1))) {
-                    int j = i + 2;
-                    while (j < length && Character.isJavaIdentifierPart(query.charAt(j))) {
-                        j++;
-                    }
-                    String name = query.substring(i + 1, j);
-                    c = '?'; // replace the parameter with a question mark
-                    i += name.length(); // skip past the end if the parameter
+            char c = namedQuery.charAt(i);
 
-                    List indexList = (List) paramMap.get(name);
-                    if (indexList == null) {
-                        indexList = new LinkedList();
-                        paramMap.put(name, indexList);
-                    }
-                    indexList.add(index);
+            //: Significa inicio de um rotulo de parametro
+            //E nao esta em uma palavra com aspas simples ou duplas
+            if (c == ':' && !inSingleQuote && !inDoubleQuote && i + 1 < length
+                    && Character.isJavaIdentifierStart(namedQuery.charAt(i + 1))) {
+                int j = i + 2;
 
-                    index++;
+                while (j < length && Character.isJavaIdentifierPart(namedQuery.charAt(j))) {
+                    j++;
                 }
+
+                String name = namedQuery.substring(i + 1, j);
+                c = '?'; // substitui o caracter c pelo parametro de index
+                i += name.length(); // pula i ate o fim do nome do parametro
+
+                List<Integer> indexList = mapPosicoesParametros.get(name);
+                //Se o parametro ainda nao existir no map inicializa-o
+                if (indexList == null) {
+                    indexList = new LinkedList<Integer>();
+                    mapPosicoesParametros.put(name, indexList);
+                }
+                indexList.add(position++);
             }
+
+            //Adiciona o novo caractere a query passada pelo parser
             parsedQuery.append(c);
-        }
 
-        // replace the lists of Integer objects with arrays of ints
-        for (Iterator itr = paramMap.entrySet().iterator(); itr.hasNext();) {
-            Map.Entry entry = (Map.Entry) itr.next();
-            List list = (List) entry.getValue();
-            int[] indexes = new int[list.size()];
-            int i = 0;
-            for (Iterator itr2 = list.iterator(); itr2.hasNext();) {
-                Integer x = (Integer) itr2.next();
-                indexes[i++] = x;
+            if (c == '\'') {
+                inSingleQuote = !inSingleQuote;
+            } else if (c == '"') {
+                inDoubleQuote = !inDoubleQuote;
             }
-            entry.setValue(indexes);
         }
-
         return parsedQuery.toString();
-    }
+    }//Fim do metodo parser
 
+    ////////////////////////////////////////////////////////////////
+    // PARAMETERS
+    ////////////////////////////////////////////////////////////////
     /**
      * Returns the indexes for a parameter.
      *
@@ -138,8 +122,8 @@ public class NamedParameterStatement {
      * @return parameter indexes
      * @throws IllegalArgumentException if the parameter does not exist
      */
-    private int[] getIndexes(String name) {
-        int[] indexes = (int[]) indexMap.get(name);
+    private List<Integer> getIndexes(String name) {
+        List<Integer> indexes = mapPosicoesParametros.get(name);
         if (indexes == null) {
             throw new IllegalArgumentException("Parameter not found: " + name);
         }
@@ -153,12 +137,11 @@ public class NamedParameterStatement {
      * @param value parameter value
      * @throws SQLException if an error occurred
      * @throws IllegalArgumentException if the parameter does not exist
-     * @see PreparedStatement#setObject(int, java.lang.Object)
+     * @see PreparedStatement#setBoolean(int, boolean)
      */
-    public void setObject(String name, Object value) throws SQLException {
-        int[] indexes = getIndexes(name);
-        for (int i = 0; i < indexes.length; i++) {
-            statement.setObject(indexes[i], value);
+    public void setBoolean(String name, boolean value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setBoolean(position, value);
         }
     }
 
@@ -169,12 +152,26 @@ public class NamedParameterStatement {
      * @param value parameter value
      * @throws SQLException if an error occurred
      * @throws IllegalArgumentException if the parameter does not exist
-     * @see PreparedStatement#setString(int, java.lang.String)
+     * @see PreparedStatement#setDate(int, java.sql.Date)
      */
-    public void setString(String name, String value) throws SQLException {
-        int[] indexes = getIndexes(name);
-        for (int i = 0; i < indexes.length; i++) {
-            statement.setString(indexes[i], value);
+    public void setDate(String name, Date value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setDate(position, value);
+        }
+    }
+
+    /**
+     * Sets a parameter.
+     *
+     * @param name parameter name
+     * @param value parameter value
+     * @throws SQLException if an error occurred
+     * @throws IllegalArgumentException if the parameter does not exist
+     * @see PreparedStatement#setDouble(int, double)
+     */
+    public void setDouble(String name, double value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setDouble(position, value);
         }
     }
 
@@ -188,9 +185,8 @@ public class NamedParameterStatement {
      * @see PreparedStatement#setInt(int, int)
      */
     public void setInt(String name, int value) throws SQLException {
-        int[] indexes = getIndexes(name);
-        for (int i = 0; i < indexes.length; i++) {
-            statement.setInt(indexes[i], value);
+        for (Integer position : getIndexes(name)) {
+            statement.setInt(position, value);
         }
     }
 
@@ -204,9 +200,53 @@ public class NamedParameterStatement {
      * @see PreparedStatement#setInt(int, int)
      */
     public void setLong(String name, long value) throws SQLException {
-        int[] indexes = getIndexes(name);
-        for (int i = 0; i < indexes.length; i++) {
-            statement.setLong(indexes[i], value);
+        for (Integer position : getIndexes(name)) {
+            statement.setLong(position, value);
+        }
+    }
+
+    /**
+     * Sets a parameter.
+     *
+     * @param name parameter name
+     * @param value parameter value
+     * @throws SQLException if an error occurred
+     * @throws IllegalArgumentException if the parameter does not exist
+     * @see PreparedStatement#setObject(int, java.lang.Object)
+     */
+    private void setObject(String name, Object value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setObject(position, value);
+        }
+    }
+
+    /**
+     * Sets a parameter.
+     *
+     * @param name parameter name
+     * @param value parameter value
+     * @throws SQLException if an error occurred
+     * @throws IllegalArgumentException if the parameter does not exist
+     * @see PreparedStatement#setString(int, java.lang.String)
+     */
+    public void setString(String name, String value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setString(position, value);
+        }
+    }
+
+    /**
+     * Sets a parameter.
+     *
+     * @param name parameter name
+     * @param value parameter value
+     * @throws SQLException if an error occurred
+     * @throws IllegalArgumentException if the parameter does not exist
+     * @see PreparedStatement#setTimestamp(int, java.sql.Time)
+     */
+    public void setTime(String name, Time value) throws SQLException {
+        for (Integer position : getIndexes(name)) {
+            statement.setTime(position, value);
         }
     }
 
@@ -220,21 +260,14 @@ public class NamedParameterStatement {
      * @see PreparedStatement#setTimestamp(int, java.sql.Timestamp)
      */
     public void setTimestamp(String name, Timestamp value) throws SQLException {
-        int[] indexes = getIndexes(name);
-        for (int i = 0; i < indexes.length; i++) {
-            statement.setTimestamp(indexes[i], value);
+        for (Integer position : getIndexes(name)) {
+            statement.setTimestamp(position, value);
         }
     }
 
-    /**
-     * Returns the underlying statement.
-     *
-     * @return the statement
-     */
-    public PreparedStatement getStatement() {
-        return statement;
-    }
-
+    ////////////////////////////////////////////////////////////////
+    // EXECUCAO DE QUERIES
+    ////////////////////////////////////////////////////////////////
     /**
      * Executes the statement.
      *
@@ -270,6 +303,35 @@ public class NamedParameterStatement {
         return statement.executeUpdate();
     }
 
+    ////////////////////////////////////////////////////////////////
+    // RECUPERACAO DE CHAVES GERADAS
+    ////////////////////////////////////////////////////////////////
+    /**
+     * Retrieves any auto-generated keys created as a result of executing this
+     * Statement object. If this Statement object did not generate any keys, an
+     * empty ResultSet object is returned.
+     *
+     * @return a ResultSet object containing the auto-generated key(s) generated
+     * by the execution of this Statement object
+     * @throws SQLException if an error occurred
+     * @see PreparedStatement#getGeneratedKeys()
+     */
+    public ResultSet getGeneratedKeys() throws SQLException {
+        return statement.getGeneratedKeys();
+    }
+
+    ////////////////////////////////////////////////////////////////
+    // OUTROS
+    ////////////////////////////////////////////////////////////////
+    /**
+     * Returns the underlying statement.
+     *
+     * @return the statement
+     */
+    public PreparedStatement getStatement() {
+        return statement;
+    }
+
     /**
      * Closes the statement.
      *
@@ -279,25 +341,4 @@ public class NamedParameterStatement {
     public void close() throws SQLException {
         statement.close();
     }
-
-    /**
-     * Adds the current set of parameters as a batch entry.
-     *
-     * @throws SQLException if something went wrong
-     */
-    public void addBatch() throws SQLException {
-        statement.addBatch();
-    }
-
-    /**
-     * Executes all of the batched statements.
-     *
-     * See {@link Statement#executeBatch()} for details.
-     *
-     * @return update counts for each statement
-     * @throws SQLException if something went wrong
-     */
-    public int[] executeBatch() throws SQLException {
-        return statement.executeBatch();
-    }
-}
+}//Fim da classe<span class="pun">;</span>
