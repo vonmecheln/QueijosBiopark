8363974aadd2edfb573a0bfbe0f6212f8092ba49
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-11-01 16:16:12 -0300

    Conexao com NamedPS e criaçaõ de tabela

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/Conexao.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/Conexao.java
@@ -1,36 +1,41 @@
 package br.com.ifprbiopark.queijo_desktop.dao;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Conexao {
-    
+
     private static Conexao unique;
-    
-    private Connection conexao;
-    
-    private Conexao(){
+
+    public Connection conexao;
+
+    private Conexao() {
         try {
             conexao = DriverManager.getConnection("jdbc:sqlite:bancoqueijos.db");
+            
+            Statement statement = conexao.createStatement();
+            // criando tabelas
+            statement.execute("CREATE TABLE IF NOT EXISTS fornecedor( ID INTEGER, NOME VARCHAR )");
+            
         } catch (SQLException ex) {
             Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public static Conexao getInstance(){
-        if (unique == null){
+    public static Conexao getInstance() {
+        if (unique == null) {
             unique = new Conexao();
         }
         return unique;
     }
-    
-    public ResultSet executarConsulta(String sql){
+
+    public ResultSet executarConsulta(String sql) {
         PreparedStatement pstm;
         ResultSet rs;
         try {
@@ -42,13 +47,21 @@ public class Conexao {
         }
         return null;
     }
-    
-    public void fecharConexao(){
+
+    public void fecharConexao() {
         try {
             conexao.close();
         } catch (SQLException ex) {
             Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    NamedParameterStatement NamedParameterStatement(String sql) throws SQLException {
+        return new NamedParameterStatement(conexao, sql);
+    }
+
+    NamedParameterStatement NamedParameterStatement(String sql, int RETURN_GENERATED_KEYS) throws SQLException {
+        return new NamedParameterStatement(conexao, sql, RETURN_GENERATED_KEYS);
     }
 
 }
