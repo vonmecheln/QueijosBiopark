1920c22d5fd893447062763134eaca123ea183dd
Luiz Gustavo Deon <luizgustavodeon@gmail.com>
2019-11-08 20:48:18 -0300

    Tela de Entrada de leite atualizada

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/view/TelaClassificacaoLeiteView.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/view/TelaClassificacaoLeiteView.java
@@ -31,11 +31,11 @@ public class TelaClassificacaoLeiteView extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGap(0, 641, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGap(0, 278, Short.MAX_VALUE)
         );
 
         pack();
