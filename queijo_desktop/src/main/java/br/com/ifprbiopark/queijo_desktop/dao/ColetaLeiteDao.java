b24cf8701a7e855abb3016f7724bb695ada30867
marcosmolinamaxicon <marcos.molina@maxiconsystems.com.br>
2019-11-14 22:07:03 -0300

    adicionado metodo consulta por id
    adicionado metodo consulta por id

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/ColetaLeiteDao.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/ColetaLeiteDao.java
@@ -103,4 +103,9 @@ public class ColetaLeiteDao extends AbstractDao<ColetaLeite> {
 
     }
 
+    @Override
+    public ColetaLeite consultar(int id) throws DbException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
