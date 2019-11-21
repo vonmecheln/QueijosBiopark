fbb9e54b30e12bd8458412ca37df51776d56abf9
Gervasio Bartnik Jr <gervasio.wd@gmail.com>
2019-11-21 20:02:17 -0300

    Correção classe Conexão para nova versão do Banco

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/Conexao.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/Conexao.java
@@ -20,100 +20,77 @@ public class Conexao {
             conexao = DriverManager.getConnection("jdbc:sqlite:bancoqueijos.db");
 
             Statement statement = conexao.createStatement();
-                   
+
             // criando tabelas
-            statement.execute("  CREATE TABLE IF NOT EXISTS atributos (" +
-                                "  idAtributo INTEGER PRIMARY KEY AUTOINCREMENT," +
-                                "  nomeAtributo VARCHAR(45) NOT NULL)");
-            
-            statement.execute("  CREATE TABLE IF NOT EXISTS pessoa (" +
-                                "  idPessoa INTEGER PRIMARY KEY AUTOINCREMENT," +
-                                "  nome VARCHAR(45) NOT NULL," +
-                                "  endereco VARCHAR(50) NOT NULL," +
-                                "  tipoFiscal VARCHAR(1) NOT NULL," +
-                                "  cadastro VARCHAR(14) NOT NULL," +
-                                "  telefone VARCHAR(15)," +
-                                "  tipoPessoa VARCHAR(45) NOT NULL)");
+            statement.execute("CREATE TABLE IF NOT EXISTS pessoa ("
+                    + "    idPessoa INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    nome VARCHAR(45) NOT NULL, "
+                    + "    endereco VARCHAR(50) NOT NULL, "
+                    + "    telefone VARCHAR (20), "
+                    + "    tipoFiscal VARCHAR(1) NOT NULL, "
+                    + "    documento VARCHAR(14) NOT NULL, "
+                    + "    tipoPessoa VARCHAR(45) NOT NULL);");
 
-            statement.execute("  CREATE TABLE IF NOT EXISTS coletaleite (" +
-                                "  idColetaLeite INTEGER PRIMARY KEY," +
-                                "  loteColeta VARCHAR(45) NOT NULL," +
-                                "  dtColeta DATE NOT NULL," +
-                                "  Produtor_idProdutor INTEGER NOT NULL REFERENCES pessoa (idPessoa)," +
-                                "  qtdLeite DOUBLE NOT NULL," +
-                                "  Pessoa_idPessoa INTEGER NOT NULL REFERENCES pessoa (idPessoa)");
-            
-            statement.execute("  CREATE TABLE IF NOT EXISTS entradaatributo (" +
-                                "  idEntradaAtributo INTEGER PRIMARY KEY," +
-                                "  valor VARCHAR(10) NOT NULL," +
-                                "  Atributos_idAtributo INTEGER NOT NULL REFERENCES atributos(idAtributo)," +
-                                "  ColetaLeite_idColetaLeite INTEGER NOT NULL REFRENCES coletaleite (idColetaLeite)");
+            statement.execute("CREATE TABLE IF NOT EXISTS atributo ("
+                    + "  idAtributo INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "  nomeAtributo VARCHAR(45) NOT NULL);");
 
-            statement.execute("  CREATE TABLE IF NOT EXISTS fermento (" +
-                                "  idFermento INTEGER PRIMARY KEY," +
-                                "  tipoFermento VARCHAR(45) NOT NULL," +
-                                "  Marca VARCHAR(45) NOT NULL)");
+            statement.execute("CREATE TABLE IF NOT EXISTS coletaleite ("
+                    + "    idColetaLeite INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    loteColeta VARCHAR(45) NOT NULL, "
+                    + "    dtColeta DATE NOT NULL, "
+                    + "    produtor_id INTEGER NOT NULL REFERENCES pessoa(idPessoa), "
+                    + "    qtdLeite NUMERIC(14,2) NOT NULL, "
+                    + "    funcionario_id INTEGER NOT NULL REFERENCES pessoa(idPessoa));");
 
-            statement.execute("  CREATE TABLE IF NOT EXISTS processamento (" +
-                                "  idProcessamento INTEGER PRIMARY KEY," +
-                                "  TipoProcessamento VARCHAR(45) NOT NULL)");
+            statement.execute("CREATE TABLE IF NOT EXISTS entradaatributo ("
+                    + "    idEntradaAtributo INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    valor VARCHAR(10) NOT NULL, "
+                    + "    atributo_id INTEGER NOT NULL REFERENCES atributo(idAtributo), "
+                    + "    coleta_id INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite));");
 
-            statement.execute("  CREATE TABLE IF NOT EXISTS tipoqueijo (" +
-                                "  idTipoQueijo INTEGER PRIMARY KEY," +
-                                "  nomeTipo VARCHAR(45) NOT NULL)");
+            statement.execute("CREATE TABLE IF NOT EXISTS fermento ("
+                    + "    idFermento INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    tipoFermento VARCHAR(45) NOT NULL, "
+                    + "    Marca VARCHAR(45) NOT NULL);");
 
-            statement.execute("  CREATE TABLE IF NOT EXISTS fabricacaoqueijo (" +
-                                "  idFabricacaoQueijo INTEGER PRIMARY KEY AUTOINCREMENT," +
-                                "  TipoQueijo_idTipoQueijo INTEGER NOT NULL REFRENCES tipoqueijo(idTipoQueijo)," +
-                                "  dataFabricacao DATE NOT NULL," +
-                                "  loteQueijo VARCHAR(45) NOT NULL," +
-                                "  ColetaLeite_idColetaLeite INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite)," +
-                                "  Processamento_idProcessamento INTEGER NOT NULL REFERENCES processamento(idProcessamento)," +
-                                "  tempoProcessamento INTEGER NOT NULL," +
-                                "  temperaturaProcessamento INTEGER NOT NULL," +
-                                "  temperaturaPreMaturacao INTEGER NOT NULL," +
-                                "  Fermento_idFermento INTEGER NOT NULL REFERENCES fermento (idFermento)," +
-                                "  qtdFermento NUMERIC(14,2) NOT NULL," +
-                                "  tempoMaturacao INTEGER NOT NULL," +
-                                "  phMaturacao NUMERIC(14,2) NOT NULL," +
-                                "  temperaturaCoagulacao INTEGER NOT NULL," +
-                                "  qtdClCalcio NUMERIC(14,2) NOT NULL," +
-                                "  qtdCoagulante NUMERIC(14,2) NOT NULL," +
-                                "  tempoCoagulacao INTEGER NOT NULL," +
-                                "  phCorte NUMERIC(14,2) NOT NULL," +
-                                "  phEnformagem NUMERIC(14,2) NOT NULL," +
-                                "  phFinal NUMERIC(14,2) NOT NULL," +
-                                "  dessoragem INTEGER NOT NULL," +
-                                "  temperaturaDessoragem INTEGER NOT NULL," +
-                                "  tipoSalga VARCHAR(45) NULL DEFAULT NULL," +
-                                "  dataMaturacao DATE NULL DEFAULT NULL," +
-                                "  temperaturaMaturacao INTEGER NULL DEFAULT NULL," +
-                                "  dataLavagem DATE NULL DEFAULT NULL," +
-                                "  tipoTratamento VARCHAR(45) NULL DEFAULT NULL," +
-                                "  tempoTratamento INTEGER NULL DEFAULT NULL," +
-                                "  qtdPecas INTEGER NOT NULL," +
-                                "  pesoMPecas NUMERIC(14,2) NOT NULL," +
-                                "  pesoTotal NUMERIC(14,2) NOT NULL," +
-                                "  observacoes LONGTEXT NULL DEFAULT NULL," +
-                                "  Responsavel_idResponsavel INTEGER NOT NULL REFERENCES pessoa (idPessoa)");
+            statement.execute("CREATE TABLE IF NOT EXISTS receitaqueijo ("
+                    + "    idReceita INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    nomeReceita VARCHAR(45) NOT NULL);");
 
-//            statement.execute("CREATE TABLE IF NOT EXISTS `pessoa` ("
-//                    + "  `idPessoa` INTEGER PRIMARY KEY,"
-//                    + "  `nome` VARCHAR(45) NOT NULL,"
-//                    + "  `endereco` VARCHAR(50) NOT NULL,"
-//                    + "  `tipoFiscal` VARCHAR(1) NOT NULL,"
-//                    + "  `cadastro` VARCHAR(14) NOT NULL,"
-//                    + "  `tipoPessoa` VARCHAR(45) NOT NULL)");
-
-//            statement.execute("CREATE TABLE IF NOT EXISTS `coletaleite` ("
-//                    + "  `idColetaLeite` INTEGER PRIMARY KEY,"
-//                    + "  `loteColeta` VARCHAR(45) NOT NULL,"
-//                    + "  `dtColeta` DATE NOT NULL,"
-//                    + "  `qtdLeite` DOUBLE NOT NULL,"
-//                    + "  `Pessoa_idPessoa` INTEGER NOT NULL,"                    
-//                    + "  CONSTRAINT `fk_ColetaLeite_Pessoa1`"
-//                    + "    FOREIGN KEY (`Pessoa_idPessoa`)"
-//                    + "    REFERENCES `mydb`.`pessoa` (`idPessoa`))");
+            statement.execute("CREATE TABLE IF NOT EXISTS fabricacaoqueijo ( "
+                    + "    idFabricacaoQueijo INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "    receita_id INTEGER NOT NULL REFERENCES receitaqueijo(idReceita), "
+                    + "    dataFabricacao DATE, "
+                    + "    loteQueijo VARCHAR(45), "
+                    + "    coleta_id INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite), "
+                    + "    qtdLeite NUMERIC(14,2) NOT NULL, "
+                    + "    TipoProcessamento VARCHAR(45) NOT NULL, "
+                    + "    tempoProcessamento INTEGER, "
+                    + "    temperaturaProcessamento INTEGER, "
+                    + "    temperaturaPreMaturacao INTEGER, "
+                    + "    fermento_id INTEGER NOT NULL REFERENCES fermento(idFermento), "
+                    + "    qtdFermento NUMERIC(14,2), "
+                    + "    tempoMaturacao INTEGER, "
+                    + "    phMaturacao NUMERIC(14,2), "
+                    + "    temperaturaCoagulacao INTEGER, "
+                    + "    qtdClCalcio NUMERIC(14,2), "
+                    + "    qtdCoagulante NUMERIC(14,2), "
+                    + "    tempoCoagulacao INTEGER, "
+                    + "    phCorte NUMERIC(14,2), "
+                    + "    phEnformagem NUMERIC(14,2), "
+                    + "    phFinal NUMERIC(14,2), "
+                    + "    dessoragem INTEGER, "
+                    + "    temperaturaDessoragem INTEGER, "
+                    + "    tipoSalga VARCHAR(45), "
+                    + "    dataMaturacao DATE, "
+                    + "    temperaturaMaturacao INTEGER, "
+                    + "    dataLavagem DATE, "
+                    + "    tipoTratamento VARCHAR(45), "
+                    + "    tempoTratamento INTEGER, "
+                    + "    qtdPecas INTEGER, "
+                    + "    pesoMPecas NUMERIC(14,2), "
+                    + "    observacoes TEXT);");
 
         } catch (SQLException ex) {
             conexao = null;
