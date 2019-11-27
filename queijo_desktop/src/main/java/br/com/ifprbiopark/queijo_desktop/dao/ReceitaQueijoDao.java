557d2a02cc073e8f67707944104fe035019fe7b3
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-11-26 22:10:34 -0300

    DAO receitaqueijo convertido para insert padrão

--- queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/ReceitaQueijoDao.java
+++ queijo_desktop/src/main/java/br/com/ifprbiopark/queijo_desktop/dao/ReceitaQueijoDao.java
@@ -10,88 +10,70 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReceitaQueijoDao extends AbstractDao<ReceitaQueijo> {
-
+    
     public ReceitaQueijoDao() throws DbException {
         super("receitaqueijo", new ArrayList<>(List.of(
                 "idReceita",
                 "nomeReceita")));
     }
-
+    
+    @Override
+    protected void confStantementInsert(NamedParameterStatement nps, ReceitaQueijo objeto) throws SQLException {
+        nps.setString("nomeReceita", objeto.getNomeTipo());
+    }
+    
     @Override
     public void inserir(ReceitaQueijo tq) throws DbException {
-        try {
-
-            String sql = "INSERT INTO receitaqueijo(nomeReceita) VALUES(:nomeReceita)";
-
-            NamedParameterStatement nps = con.NamedParameterStatement(sql);
-            nps.setString("nomeReceita", tq.getNomeTipo());
-
-            int exec = nps.executeUpdate();
-            if (exec == 0) {
-                throw new NotExecuteInsertException();
-            }
-
-            int key = 0;
-            ResultSet retorno = nps.getGeneratedKeys();
-            if (retorno != null && retorno.next()) {
-                key = retorno.getInt(1);
-            } else {
-                throw new GeneratedKeysException();
-            }
-            tq.setIdTipoQueijo(key);
-
-        } catch (SQLException ex) {
-            throw new DbException(ex);
-        }
+        InserirDefault(tq);
     }
-
+    
     @Override
     public boolean excluir(ReceitaQueijo tq) throws DbException {
         try {
-
+            
             String sql = "DELETE receitaqueijo WHERE idReceita = :id";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
             nps.setInt("id", tq.getIdTipoQueijo());
-
+            
             boolean exec = nps.execute();
             if (!exec) {
                 throw new SQLException();
             } else {
                 return exec;
             }
-
+            
         } catch (SQLException ex) {
             throw new DbException(ex);
         }
     }
-
+    
     @Override
     public ReceitaQueijo consultar(ReceitaQueijo tq) throws DbException {
         return consultar(tq.getIdTipoQueijo());
     }
-
+    
     @Override
     public ReceitaQueijo alterar(ReceitaQueijo tq) throws DbException {
         try {
-
+            
             String sql = "UPDATE receitaqueijo SET nomeReceita = :nomeReceita WHERE idReceita = :id";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
             nps.setString("nomeReceita", tq.getNomeTipo());
             nps.setInt("id", tq.getIdTipoQueijo());
-
+            
             int exec = nps.executeUpdate();
             if (exec == 0) {
                 throw new NotExecuteInsertException();
             }
             return tq;
-
+            
         } catch (SQLException ex) {
             throw new DbException(ex);
         }
     }
-
+    
     @Override
     public ReceitaQueijo consultar(int id) throws DbException {
         try {
@@ -99,11 +81,11 @@ public class ReceitaQueijoDao extends AbstractDao<ReceitaQueijo> {
 
             //String SQL;
             String sql = "SELECT idReceita, nomeReceita FROM receitaqueijo WHERE idReceita = :id ";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
-
+            
             nps.setInt("id", id);
-
+            
             ResultSet consulta = nps.executeQuery();
             while (consulta.next()) {
                 tq.setIdTipoQueijo(consulta.getInt("idReceita"));
@@ -115,16 +97,16 @@ public class ReceitaQueijoDao extends AbstractDao<ReceitaQueijo> {
             throw new DbException(ex);
         }
     }
-
+    
     public List<ReceitaQueijo> consultar() throws DbException {
         try {
             List<ReceitaQueijo> lista = new ArrayList<ReceitaQueijo>();
 
             //String SQL;
             String sql = "SELECT idReceita, nomeReceita FROM receitaqueijo";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
-
+            
             ResultSet consulta = nps.executeQuery();
             while (consulta.next()) {
                 ReceitaQueijo tq = new ReceitaQueijo();
@@ -138,17 +120,17 @@ public class ReceitaQueijoDao extends AbstractDao<ReceitaQueijo> {
             throw new DbException(ex);
         }
     }
-
+    
     public List<ReceitaQueijo> consultar(String nome) throws DbException {
         try {
             List<ReceitaQueijo> lista = new ArrayList<ReceitaQueijo>();
 
             //String SQL;
             String sql = "SELECT idReceita, nomeReceita FROM receitaqueijo WHERE nomeReceita LIKE '%:nomeReceita%'";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
             nps.setString("nomeReceita", nome);
-
+            
             ResultSet consulta = nps.executeQuery();
             while (consulta.next()) {
                 ReceitaQueijo tq = new ReceitaQueijo();
@@ -162,29 +144,29 @@ public class ReceitaQueijoDao extends AbstractDao<ReceitaQueijo> {
             throw new DbException(ex);
         }
     }
-
+    
     public List<ReceitaQueijo> listarColetas() {
         try {
             List<ReceitaQueijo> tipoQueijo = new ArrayList<>();
             String sql = "SELECT idReceita, nomeReceita FROM receitaqueijo";
-
+            
             NamedParameterStatement nps = con.NamedParameterStatement(sql);
-
+            
             ResultSet consulta = nps.executeQuery();
             while (consulta.next()) {
                 ReceitaQueijo tipo = new ReceitaQueijo();
                 tipo.setIdTipoQueijo(consulta.getInt("idReceita"));
                 tipo.setNomeTipo(consulta.getString("nomeReceita"));
-
+                
                 tipoQueijo.add(tipo);
-
+                
             }
             return tipoQueijo;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
-
+        
     }
-
+    
 }
