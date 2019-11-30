b9ba6074e5a32f04e066ffddc4264c5029b7f6f1
Gervasio Bartnik Jr <gervasio.wd@gmail.com>
2019-11-29 21:13:25 -0300

    Atualizados os scripts "create" e "insert"
    - incluido campo 'inativo' em todas as tabelas;
    - alterado o campo 'fermento.tipoFermento' para 'fermento.nome';
    - alterado o campo 'fabricacaoqueijo.tipoProcessamento' para 'fabricacaoqueijo.tipoLeite';
    - alterado o campo 'fabricacaoqueijo.tempoMaturacao' para 'fabricacaoqueijo.tempoFermentacao';
    - Adequado script de inserção para nova versão do banco;

--- queijo_desktop/createTable1129.sql
+++ queijo_desktop/createTable1129.sql
@@ -1,70 +1,80 @@
-CREATE TABLE IF NOT EXISTS pessoa ( 
+CREATE TABLE IF NOT EXISTS pessoa (
     idPessoa INTEGER PRIMARY KEY AUTOINCREMENT, 
     nome VARCHAR(45) NOT NULL, 
     endereco VARCHAR(50) NOT NULL, 
-    telefone VARCHAR (20),
+    telefone VARCHAR (20), 
     tipoFiscal VARCHAR(1) NOT NULL, 
     documento VARCHAR(14) NOT NULL, 
-    tipoPessoa VARCHAR(45) NOT NULL);
+    tipoPessoa VARCHAR(45) NOT NULL,
+    inativo INTEGER DEFAULT(0));
 
 CREATE TABLE IF NOT EXISTS atributo (
-  idAtributo INTEGER PRIMARY KEY AUTOINCREMENT,
-  nomeAtributo VARCHAR(45) NOT NULL);
+    idAtributo INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeAtributo VARCHAR(45) NOT NULL,
+    inativo INTEGER DEFAULT(0));
 
-CREATE TABLE IF NOT EXISTS coletaleite ( 
+CREATE TABLE IF NOT EXISTS coletaleite (
     idColetaLeite INTEGER PRIMARY KEY AUTOINCREMENT, 
     loteColeta VARCHAR(45) NOT NULL, 
     dtColeta DATE NOT NULL, 
     produtor_id INTEGER NOT NULL REFERENCES pessoa(idPessoa), 
     qtdLeite NUMERIC(14,2) NOT NULL, 
-    funcionario_id INTEGER NOT NULL REFERENCES pessoa(idPessoa));
+    funcionario_id INTEGER NOT NULL REFERENCES pessoa(idPessoa), 
+    situacao VARCHAR(15),
+    inativo INTEGER DEFAULT(0));
 
-CREATE TABLE IF NOT EXISTS entradaatributo ( 
+CREATE TABLE IF NOT EXISTS entradaatributo (
     idEntradaAtributo INTEGER PRIMARY KEY AUTOINCREMENT, 
     valor VARCHAR(10) NOT NULL, 
     atributo_id INTEGER NOT NULL REFERENCES atributo(idAtributo), 
-    coleta_id INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite));
+    coleta_id INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite),
+    inativo INTEGER DEFAULT(0));
 
-
-CREATE TABLE IF NOT EXISTS fermento ( 
+CREATE TABLE IF NOT EXISTS fermento (
     idFermento INTEGER PRIMARY KEY AUTOINCREMENT, 
-    tipoFermento VARCHAR(45) NOT NULL, 
-    Marca VARCHAR(45) NOT NULL);
+    nome VARCHAR(45) NOT NULL, 
+    marca VARCHAR(45) NOT NULL,
+    inativo INTEGER DEFAULT(0));
 
-CREATE TABLE IF NOT EXISTS receitaqueijo ( 
-    idReceita INTEGER PRIMARY KEY AUTOINCREMENT, 
-    nomeReceita VARCHAR(45) NOT NULL);
+CREATE TABLE IF NOT EXISTS receitaqueijo (
+    idReceita INTEGER PRIMARY KEY AUTOINCREMENT,
+    nomeReceita VARCHAR(45) NOT NULL,
+    inativo INTEGER DEFAULT(0));
     
 CREATE TABLE IF NOT EXISTS fabricacaoqueijo ( 
     idFabricacaoQueijo INTEGER PRIMARY KEY AUTOINCREMENT, 
     receita_id INTEGER NOT NULL REFERENCES receitaqueijo(idReceita), 
-    dataFabricacao DATE, 
+    dataFabricacao DATE NOT NULL,
     loteQueijo VARCHAR(45), 
     coleta_id INTEGER NOT NULL REFERENCES coletaleite(idColetaLeite), 
-    qtdLeite NUMERIC(14,2) NOT NULL,
-    TipoProcessamento VARCHAR(45) NOT NULL,
-    tempoProcessamento INTEGER , 
-    temperaturaProcessamento INTEGER , 
-    temperaturaPreMaturacao INTEGER , 
-    fermento_id INTEGER NOT NULL REFERENCES fermento(idFermento), 
-    qtdFermento NUMERIC(14,2) , 
-    tempoMaturacao INTEGER , 
-    phMaturacao NUMERIC(14,2) , 
-    temperaturaCoagulacao INTEGER , 
-    qtdClCalcio NUMERIC(14,2) , 
-    qtdCoagulante NUMERIC(14,2) , 
-    tempoCoagulacao INTEGER , 
-    phCorte NUMERIC(14,2) , 
-    phEnformagem NUMERIC(14,2) , 
-    phFinal NUMERIC(14,2) , 
-    dessoragem INTEGER , 
-    temperaturaDessoragem INTEGER , 
-    tipoSalga VARCHAR(45) , 
-    dataMaturacao DATE , 
-    temperaturaMaturacao INTEGER , 
-    dataLavagem DATE , 
-    tipoTratamento VARCHAR(45) , 
-    tempoTratamento INTEGER , 
-    qtdPecas INTEGER , 
-    pesoMPecas NUMERIC(14,2) , 
-    observacoes TEXT);
+    qtdLeite NUMERIC(14,2) NOT NULL, 
+    tipoLeite VARCHAR(45), 
+    tempoProcessamento INTEGER, 
+    temperaturaProcessamento INTEGER, 
+    temperaturaPreMaturacao INTEGER, 
+    phMaturacao NUMERIC(14,2), 
+    fermento_id INTEGER REFERENCES fermento(idFermento), 
+    qtdFermento NUMERIC(14,2), 
+    tempoFermentacao INTEGER, 
+    temperaturaCoagulacao INTEGER, 
+    qtdClCalcio NUMERIC(14,2), 
+    qtdCoagulante NUMERIC(14,2), 
+    tempoCoagulacao INTEGER, 
+    phCorte NUMERIC(14,2), 
+    phEnformagem NUMERIC(14,2), 
+    phFinal NUMERIC(14,2),
+    dessoragem INTEGER, 
+    temperaturaDessoragem INTEGER,
+    tipoSalga VARCHAR(45),
+    dataMaturacao DATE,
+    temperaturaMaturacao INTEGER, 
+    dataLavagem DATE,
+    tipoTratamento VARCHAR(45), 
+    tempoTratamento INTEGER,
+    qtdPecas INTEGER, 
+    pesoMPecas NUMERIC(14,2),
+    pesoTotal NUMERIC(14,2), 
+    observacoes TEXT,
+    loteAcabado VARCHAR(45), 
+    responsavel_id INTEGER REFERENCES pessoa(idPessoa),
+    inativo INTEGER DEFAULT(0));
--- queijo_desktop/populaBanco2.sql
+++ queijo_desktop/populaBanco2.sql
@@ -12,10 +12,10 @@ INSERT INTO receitaqueijo (nomeReceita) VALUES ('Queijo Artesanal');
 INSERT INTO receitaqueijo (nomeReceita) VALUES ('Queijo Reblochon');
 
 -- Populando tabela Fermento
-INSERT INTO fermento (tipofermento, marca) VALUES ('Mesofilico old', 'Docina');
-INSERT INTO fermento (tipofermento, marca) VALUES ('Mesofilico termofilico', 'Docina');
-INSERT INTO fermento (tipofermento, marca) VALUES ('Mesofilico tipo O', 'Docina');
-INSERT INTO fermento (tipofermento, marca) VALUES ('Mesofilico +PPR10', 'Docina+Lactopar');
+INSERT INTO fermento (nome, marca) VALUES ('Mesofilico old', 'Docina');
+INSERT INTO fermento (nome, marca) VALUES ('Mesofilico termofilico', 'Docina');
+INSERT INTO fermento (nome, marca) VALUES ('Mesofilico tipo O', 'Docina');
+INSERT INTO fermento (nome, marca) VALUES ('Mesofilico +PPR10', 'Docina+Lactopar');
 
 -- Populando tabela Pessoa
 INSERT INTO pessoa (nome, endereco, telefone, tipoFiscal, documento, tipoPessoa) VALUES ('Beatriz Durlan', 'Rua Roxo nr 11','45 3333-3333','F', '00000000000','Produtor');
@@ -63,31 +63,8 @@ INSERT INTO entradaatributo (valor, atributo_id, coleta_id) VALUES ('4,11',3,5);
 INSERT INTO entradaatributo (valor, atributo_id, coleta_id) VALUES ('3,24',4,5);
 
 -- Populando tabela FabricacaoQueijo
-INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, TipoProcessamento, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoMaturacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (1,'2019-06-27','270919SP',1,30,'Pasteurizado',30,63,36,1,1.5,60,6.81,0,14.4,21,40,6.87,0,0,0,0,'Salmora','2019-06-28',12,'2019-06-29','Outro',60,7,800,'Utilizado 10L de agua aquecida e retirado 15L de soro');
-INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, TipoProcessamento, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoMaturacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (2,'2019-07-02','0207196D',2,30,'Pasteurizado',30,65,36,1,1.5,60,6.71,34,14.5,21,40,6.68,6.69,0,0,0,'Nenhuma','2019-07-03',12,'2019-07-03','Outro',60,1,3000,'');
-INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, TipoProcessamento, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoMaturacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (3,'2019-07-04','040719TOMME',3,31,'Leite Cru',0,0,38,2,1.5,60,6.53,0,0,18,40,6.54,0,0,0,0,'Nenhuma','2019-06-28',12,'2019-07-05','Outro',60,1,3100,'Corte com faca, adição de sal na superfície, colocado na cerveja dia 05/07');
-INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, TipoProcessamento, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoMaturacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (4,'2019-07-09','090719MB',3,30,'Pasteurizado',30,65,38,2,1.5,60,6.73,34,13.75,19.25,40,6.45,6.35,4.99,0,0,'Nenhuma','2019-07-10',12,'2019-07-11','Diário',60,3,980,'Utilização de leite de Jersey');
-INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, TipoProcessamento, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoMaturacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (5,'2019-07-11','110719',1,30,'Pasteurizado',30,65,36,3,1.5,60,6.69,34,14.5,21,40,6.72,6.7,6.52,0,0,'Salmora','2019-07-12',14,'2019-07-12','Outro',60,12,491,'10 peças de 290g + 2 peças de 1,5kg');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, tipoLeite, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoFermentacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (1,'2019-06-27','270919SP',1,30,'Pasteurizado',30,63,36,1,1.5,60,6.81,0,14.4,21,40,6.87,0,0,0,0,'Salmora','2019-06-28',12,'2019-06-29','Outro',60,7,800,'Utilizado 10L de agua aquecida e retirado 15L de soro');
+INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, tipoLeite, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoFermentacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (2,'2019-07-02','0207196D',2,30,'Pasteurizado',30,65,36,1,1.5,60,6.71,34,14.5,21,40,6.68,6.69,0,0,0,'Nenhuma','2019-07-03',12,'2019-07-03','Outro',60,1,3000,'');
+INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, tipoLeite, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoFermentacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (3,'2019-07-04','040719TOMME',3,31,'Leite Cru',0,0,38,2,1.5,60,6.53,0,0,18,40,6.54,0,0,0,0,'Nenhuma','2019-06-28',12,'2019-07-05','Outro',60,1,3100,'Corte com faca, adição de sal na superfície, colocado na cerveja dia 05/07');
+INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, tipoLeite, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoFermentacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (4,'2019-07-09','090719MB',3,30,'Pasteurizado',30,65,38,2,1.5,60,6.73,34,13.75,19.25,40,6.45,6.35,4.99,0,0,'Nenhuma','2019-07-10',12,'2019-07-11','Diário',60,3,980,'Utilização de leite de Jersey');
+INSERT INTO fabricacaoqueijo (receita_id, dataFabricacao, loteQueijo, coleta_id, qtdLeite, tipoLeite, tempoProcessamento, temperaturaProcessamento,temperaturaPreMaturacao, fermento_id, qtdFermento, tempoFermentacao, phMaturacao, temperaturaCoagulacao, qtdClCalcio, qtdCoagulante, tempoCoagulacao, phCorte,phEnformagem, phFinal, dessoragem, temperaturaDessoragem, tipoSalga, dataMaturacao, temperaturaMaturacao, dataLavagem, tipoTratamento, tempoTratamento, qtdPecas,pesoMPecas, observacoes) VALUES (5,'2019-07-11','110719',1,30,'Pasteurizado',30,65,36,3,1.5,60,6.69,34,14.5,21,40,6.72,6.7,6.52,0,0,'Salmora','2019-07-12',14,'2019-07-12','Outro',60,12,491,'10 peças de 290g + 2 peças de 1,5kg');
